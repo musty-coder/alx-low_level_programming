06d5971d6f6e03c562b6c7cc61e78b3a70504165
musty-coder <mustyboii2017@gmail.com>
2022-12-09 06:27:07 -0800

    Help, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-  char mystr1[] = "\"Programming is like building a multilingual puzzle";
+char mystr1[] = "\"Programming is like building a multilingual puzzle";
 
-  puts(mystr1);
-
-  return 0;
+puts(mystr1);
+ 
+return (0);
 }
