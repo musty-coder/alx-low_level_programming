6f22425818e697a2dcadcdbd2e968e8a6c269edf
musty-coder <mustyboii2017@gmail.com>
2022-12-12 11:09:52 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,37 +7,27 @@
 
 
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
-  
+
 {
-  
-  char low;
-  
 
-  
-  for (low = 'a'; low <= 'z'; low++)
-    
-    putchar(low);
-  
-  for (low = 'A'; low <= 'Z'; low++)
-    
-    putchar(low);
-  
-  putchar('\n');
-  
+char low;
 
-  
-  return (0);
-  
+for (low = 'a'; low <= 'z'; low++)
+
+putchar(low);
+
+for (low = 'A'; low <= 'Z'; low++)
+
+putchar(low);
+
+putchar('\n');
+
+return (0);
+
 }
