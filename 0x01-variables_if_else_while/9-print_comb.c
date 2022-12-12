3f7f6a4aaef543f61692b07fc4fcb03793412a26
musty-coder <mustyboii2017@gmail.com>
2022-12-12 12:03:50 -0800

    Combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -17,13 +17,27 @@ int main(void)
 {
 
 int d;
-  
+
 for (d = '0'; d <= '9'; d++)
+
+{
 
 putchar(d);
 
+if (d != '9')
+
+{
+
+putchar(',');
+
+putchar(' ');
+
+}
+
+}
+
 putchar('\n');
-  
+
 return (0);
 
 }
