fec6e173b718edf05bb6a636e4d099342878d42a
musty-coder <mustyboii2017@gmail.com>
2022-12-12 13:10:47 -0800

    The success

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,8 +4,6 @@
 
 #include <time.h>
 
-
-
 /**
 * main - Entry point
 *
@@ -16,17 +14,19 @@ int main(void)
 
 {
 
-int d, p;
+int d, p, q;
 
 for (d = '0'; d < '9'; d++)
 
 {
-
 for (p = d + 1; p <= '9'; p++)
 
 {
+for (q = p + 1; q <= '9'; q++)
 
-if (p != d)
+{
+
+if ((p != d) != q)
 
 {
 
@@ -34,7 +34,9 @@ putchar(d);
 
 putchar(p);
 
-if (d == '8' && p == '9')
+putchar(q);
+
+if (d == '7' && p == '8')
 
 continue;
 
@@ -43,9 +45,8 @@ putchar(',');
 putchar(' ');
 
 }
-
 }
-
+}
 }
 
 putchar('\n');
