9f95f65bd2dfbecc95c03f7bc57b474488bc8c2f
musty-coder <mustyboii2017@gmail.com>
2023-01-04 06:23:07 -0800

    3 main

---
+++ 0x08-recursion/3-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+int factorial(int);
+
+
+
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+
+{
+
+int r;
+
+r = factorial(1);
+
+printf("%d\n", r);
+
+return (0);
+
+}
